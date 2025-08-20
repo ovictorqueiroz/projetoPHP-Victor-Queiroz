13609c3274530fd7de412f43ec0dbe26e2bbbb43
ovictorqueiroz <victorbr442@gmail.com>
2025-08-19 21:58:38 -0300

    Operadores PHP

--- operadores.php
+++ operadores.php
@@ -1,11 +1,33 @@
 <?php 
 
 	echo "Trabalhando com operadores em PHP <br>";
-	echo "Operadores de soma <br>";
-	echo "Operadores de subtração <br>";
-	echo "Operadores de multiplicação <br>";
-	echo "Operadores de divisão <br>";
 
-	echo "Insira a operação desejada: <br>";
+	echo "<br>";
+
+	echo "Operadores de soma: + <br>";
+	echo "Operadores de subtração: - <br>";
+	echo "Operadores de multiplicação: * <br>";
+	echo "Operadores de divisão: / <br>";
+	echo "Operadores de módulo: % <br>";
+
+	echo "<br>";
+
+	echo "Operadores de comparação <br>";
+	echo "Igual: A == b <br>";
+	echo "Maior que: A > b <br>";
+	echo "Menor que: A < b <br>";
+	echo "Maior ou igual a: A >= b <br>";
+	echo "Menor ou igual a: A <= b <br>";
+
+	echo "<br>";
+
+	echo "Contadores <br>";
+	echo "A++ -> A = A + 1 <br>";
+	echo "A-- -> A = A - 1 <br>";
+	echo "++A -> A = 1 + A <br>";
+	echo "--A -> A = 1 - A <br>";
+
+
+	
 
  ?>
