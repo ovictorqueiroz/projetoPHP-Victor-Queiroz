984aee75ad038a077c2c85aa5ec598727f7ee9fb
ovictorqueiroz <victorbr442@gmail.com>
2025-08-26 19:36:56 -0300

    trabalhando com funcoes

---
+++ funcoes-php.php
@@ -0,0 +1,15 @@
+<?php 
+	// Criando uma função
+	function somaValor (){
+
+		$n1 = 10;
+		$n2 = 10;
+
+		$resposta = $n1 + $n2;
+		
+		echo "A soma de $n1 + $n2 é $resposta";
+	}
+
+	somaValor();
+
+ ?>
