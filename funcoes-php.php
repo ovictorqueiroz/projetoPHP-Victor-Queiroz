894950b2cd790056ac6289101183a7282e67c199
ovictorqueiroz <victorbr442@gmail.com>
2025-08-26 20:09:40 -0300

    trabalhando com funcoes

--- funcoes-php.php
+++ funcoes-php.php
@@ -1,5 +1,23 @@
 <?php 
-	// Criando uma função com parâmetros
+	/*
+
+	// Criando uma função 'fechada'
+	function somaValor (){
+
+		$n1 = 10;
+		$n2 = 10;
+
+		$resposta = $n1 + $n2;
+		
+		echo "A soma de $n1 + $n2 é $resposta";
+	}
+
+	// Chamando/ executando a função 
+	somaValor();
+	*/
+
+	/*
+		// Criando uma função com parâmetros
 	function somaValor ($n1, $n2){
 
 		$resposta = $n1 + $n2;
@@ -12,6 +30,23 @@
 	$valor2 = 20;
 
 
-	somaValor($valor1, $valor2);
+	somaValor($valor1, $valor2)
+	*/
+
+	// Criando uma função com 'return', para deixar a função ainda mais enxuta
+	function somaValor ($n1, $n2){
+
+	return ($n1 + $n2);
+	
+	}
+
+
+	$valor1 = 100;
+	$valor2 = 20;
+
+
+	$resposta = somaValor($valor1, $valor2);
+
+	echo "A soma de $valor1 + $valor2 é $resposta";
 
  ?>
