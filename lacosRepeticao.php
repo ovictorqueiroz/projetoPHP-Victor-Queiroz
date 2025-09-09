3feb2a44a589a1e44d83e20a328a2de32d30dd6a
ovictorqueiroz <victorbr442@gmail.com>
2025-09-09 20:05:12 -0300

    exercicio 1 resolvido

--- lacosRepeticao.php
+++ lacosRepeticao.php
@@ -1,3 +1,14 @@
 <?php 
-	echo "teste";
+	// Exercicio 1
+	$n1 = 5;
+	$n2 = 5;
+	$soma = $n1 + $n2;
+
+	if($soma > 20){
+		$soma += 8;
+		echo "O resultado somado a 8 é $soma";
+	} else{
+		$soma -= 5;
+		echo "O resultado subtraindo 5 é $soma";
+	}
  ?>
