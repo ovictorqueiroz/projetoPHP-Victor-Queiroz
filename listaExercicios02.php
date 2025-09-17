5d5978bc3657174d317705a1a6c9ea1d77c4d051
ovictorqueiroz <victorbr442@gmail.com>
2025-09-16 21:17:15 -0300

    listaExercicios02

--- listaExercicios02.php
+++ listaExercicios02.php
@@ -1,11 +1,28 @@
 <?php 
 	//EXERCICIO 01
 	
+	/*
 	//EXERCICIO 02
-	
-
 	for( $cont = 0; $cont <= 100; $cont++){
-		echo "$cont <br>";
+
+		if($cont % 10 == 0){
+			echo "$cont é múltiplo de 10 <br>";
+		}
+		else{
+			echo "$cont <br>";
+		}
+		
 	}
+	*/
+
+	//EXERCICIO 03
+		$grao = 1;
+
+	for($casa = 1; $casa <= 64; $casa++){
+		
+		echo "Casa $casa: $grao grãos de trigo<br>";
+		$grao = $grao * 2;
+	}
+
 
  ?>
