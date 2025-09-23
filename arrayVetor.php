a0fc4a6c3a6d00ba0bad72cd2ff1575a09e83f91
ovictorqueiroz <victorbr442@gmail.com>
2025-09-23 19:40:19 -0300

    estudando arrays no php

--- arrayVetor.php
+++ arrayVetor.php
@@ -1,9 +1,19 @@
 <?php 
+
+//Criando array usando a função:
 $alunos = array();
 
 $alunos[0] = 'Maria';
 $alunos[1] = 'João';
 
-echo ($alunos[0].$alunos[1]);
+echo ($alunos[0]. "<br>");
+echo ($alunos[1]. "<br>");
+
+//Criando array e já atribuindo valores:
+$cores = array("vermelho", "amarelo", "azul");
+
+echo($cores[0] . $cores[1] . $cores[2]);
+
+
 
  ?>
