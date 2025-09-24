cf5fc272a2c7e82c6b13ccda5a9ce1a676106f99
ovictorqueiroz <victorbr442@gmail.com>
2025-09-23 21:44:15 -0300

    atualizacao de exemplos

--- arrayMatriz.php
+++ arrayMatriz.php
@@ -1,10 +1,38 @@
 <?php 
-//Exemplo 1
-$nomeAlunos = ['Maria', 'João', 'Pedro', 'Paulo', 'Lucas']
-foreach ($nome as $nomes) {
-	echo "<br> . $nomes";
+//Exemplo1
+$nomeAlunos = ['Maria', 'João', 'Pedro', 'Paulo', 'Lucas'];
+
+
+
+foreach ($nomeAlunos as $nomes) {
+	echo "<br> $nomes";
 }
 
 //Exemplo2
 $numeros = [1,2,3,4,5];
+
+foreach($numeros as $valor){
+	echo "<br> Valor é $valor";
+}
+
+echo "<br>";
+
+//Exemplo3 
+
+$usernames["MA"] = "Maria";
+$usernames["PE"] = "Pedro";
+$usernames["MU"] = "Maurício";
+
+echo "<pre>";
+	var_dump($usernames);
+echo "</pre>";
+
+
+//Exemplo4
+$s = ['João' => 1000, 'Maria' => 2000, 'José' => 6000];
+
+foreach ($s as $salario){
+	echo "O Salário de João é $salario";
+}
+
 ?>
