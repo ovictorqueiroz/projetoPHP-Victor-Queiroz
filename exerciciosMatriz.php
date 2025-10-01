be35d2529e57f07a0a78d7752537ee1ed513a2a0
ovictorqueiroz <victorbr442@gmail.com>
2025-09-30 21:39:44 -0300

    Exercicios de Matrizes no PHP

--- exerciciosMatriz.php
+++ exerciciosMatriz.php
@@ -1,6 +1,8 @@
 <?php 
-
+	/*
 	//[EXERCICIO 01]
+	echo("Exercicio 1 <br>");
+
 	$m1 = [
 		[25, 12, 35],
 		[85, 47, 98],
@@ -32,13 +34,6 @@
 		echo("<br>");
 	}
 
-	/*
-	$m3 = [
-		[],
-		[],
-		[]
-	];
-	*/
 
 	echo"<br>";
 
@@ -66,7 +61,79 @@
 		}
 		echo("<br>");
 	}
+
+	//[EXERCICIO 02]
+	echo "<br> Exercicio 02 <br>";
 	
+	$somaM1= 0; $somaM2= 0; $somaM3 = 0;
+
+	for($l = 0; $l < 3; $l++){
+		for ($i=0; $i < 3; $i++) {
+
+			$somaM1 += $m1[$l][$i];
+			$somaM2 += $m2[$l][$i];
+			$somaM3 += $m3[$l][$i];
+
+
+		}
+	}
 	
+	echo "A soma da Matriz 1 é: $somaM1 <br>";
+	echo "A soma da Matriz 2 é: $somaM2 <br>";
+	echo "A soma da Matriz 3 é: $somaM3 <br>";
+	*/
+
+	/*
+	//[EXERCICIO 03]
+	echo "<br> EXERCICIO 03 <br>";
+
+	$alunos = [
+		["nome" => "Ana", "nota" => 8],
+		["nome" => "Bernardo", "nota" => 7.5],
+		["nome" => "Cláudio", "nota" =>7],
+		["nome" => "Deyse", "nota" =>6],
+		["nome" => "Emanuel", "nota" =>5],
+		["nome" => "Fabrício", "nota" =>9],
+		["nome" => "Guilherme", "nota" =>9.5],
+		["nome" => "Heitor", "nota" =>6.5],
+		["nome" => "Igor", "nota" =>8.5],
+		["nome" => "Jacó", "nota" => 10],
+	];
+
+	for($l = 0; $l < 10; $l++){
+		for ($i=0; $i < 2; $i++) {
+			for ($n=0; $n < ; $n++) { 
+				
+			}
+			
+		}
+		echo "<br>";
+	}
+	*/
+
+	/*
+	//[EXERCICIO 04]
+	echo "<br> EXERCICIO 04 <br>";
+
+	$ano = [
+		1 => "Janeiro",
+		2 => "Fevereiro",
+		3 => "Março",
+		4 => "Abril",
+		5 => "Maio",
+		6 => "Junho",
+		7 => "Julho",
+		8 => "Agosto",
+		9 => "Setembro",
+		10 => "Outubro",
+		11 => "Novembro",
+		12 => "Dezembro",
+	];
+
+	$input = 5;
+
+	echo $ano[$input];
+	*/
+
 
 ?>
