2eaf561548cd2020b48b784bc2431f367677cb5a
ovictorqueiroz <victorbr442@gmail.com>
2025-10-14 19:54:37 -0300

    integrando formularios com o backend

--- cadastrar.php
+++ cadastrar.php
@@ -1,4 +1,13 @@
 <?php 	
- 		echo("Cadastrado com sucesso!");
+ 	$userName = $_POST['userName'];
+ 	$userEmail = $_POST['userEmail'];
+ 	$userPass = $_POST['userPass'];
+
+ 	if($userName == 'Pedrinho123' and $userPass == 123456){
+ 		echo("Bem-Vindo, $userName!");
+ 	}
+ 	else{
+ 		echo("Usuário ou senha inválidos!");
+ 	}
+
  ?>
- 
